e65c726badb2cf1eab40322e443e28960818602e
Андрей Коленков <avoknelok@mail.ru>
2022-11-15 01:09:29 +0300

    Пытаюсь юзать execute, но пока что проблема в запросе

--- data_collector/sources/data_fetcher.py
+++ data_collector/sources/data_fetcher.py
@@ -11,7 +11,7 @@ TIMEOUT = 10
 
 
 def make_request(vk_token, api_version, method, parameters):
-    '''Делает запрос и пишет данные в кэш'''
+    '''Делает запрос в vk api'''
     base_url = "https://api.vk.com/method/"
     param_dict = parameters
     param_dict.update({"v": api_version, "access_token": {vk_token}})
@@ -29,14 +29,18 @@ def make_request(vk_token, api_version, method, parameters):
 
 
 def generate_data_to_cache(vk_token, api_version, group_ids, cache_filename):
+    '''Пишет данные в кэш'''
     with open(cache_filename, "w", encoding="UTF-8") as file:
         file.write("{")
         for group_id in group_ids:
             posts = get_wall_post_links(vk_token, api_version, group_id)
-            for post_id, post_text in posts:
-                post_comments = get_post_text_comments(
-                    vk_token, api_version, group_id, post_id)
-                string_to_file = f"{json.dumps({post_text: post_comments}, ensure_ascii=False, indent=4).replace('{','').replace('}','')},"
+            for i in range(0, len(posts), 25):
+                cur_post_ids, cur_post_texts = (
+                    [str(x[0]) for x in posts[i:i+25]], [x[1] for x in posts[i:i+25]])
+                posts_comments = get_post_text_comments_25(
+                    vk_token, api_version, group_id, cur_post_ids)
+                post_text_pairs = zip(cur_post_texts, posts_comments)
+                string_to_file = f"{json.dumps(dict(post_text_pairs), ensure_ascii=False, indent=4).replace('{','').replace('}','')},"
                 file.write(string_to_file)
     with open(cache_filename, 'rb+') as file:  # Хочу удалить последний символ
         file.seek(-1, os.SEEK_END)
@@ -71,29 +75,21 @@ def get_wall_post_links(vk_token, api_version, group_id):
     return posts
 
 
-def get_post_text_comments(vk_token, api_version, group_id, post_id):
-    '''Возвращает список текстов комментариев по айди на пост'''
+def get_post_text_comments_25(vk_token, api_version, group_id, post_ids):
+    vkscript_code = f"""
+var comments = [];
+var post_ids = {post_ids};
+for (const post_id of post_ids){{
+comments.push(API.wall.getComments({{'owner_id':'{group_id}','post_id':post_id}}));
+}}
+return comments;
+"""
+    print(vkscript_code)
     try:
-        comments = make_request(vk_token, api_version, "wall.getComments", {
-            "owner_id": group_id, "post_id": post_id, "count": 1})
+        comments_list = make_request(vk_token, api_version, "execute", {
+                                     "code": vkscript_code})
     except RuntimeError as exception:
         logging.info(f"Error occured while fetching comments:\n{exception}")
         return []
-    comment_count = int(comments["response"]["count"])
-    comments_text = []
-    logging.info(
-        f"Start revieve of texts of number of comments: {comment_count}")
-    for i in range(0, comment_count, STRIDE):
-        try:
-            comments = make_request(vk_token, api_version, "wall.getComments", {
-                "owner_id": group_id, "post_id": post_id, "count": STRIDE, "offset": i})
-        except RuntimeError as exception:
-            logging.info(f"Error occured while fetching comments:\n{exception}")
-            return []
-        comments_items = comments["response"]["items"]
-        comments_text.extend([comments_item["text"]
-                             for comments_item in comments_items])
-        logging.info(
-            f"Got new comments batch. Total comment (for this post) count: {len(comments_text)}")
-
-    return comments_text
+    comments_items = comments_list["response"]["items"]
+    return [comments_item["text"] for comments_item in comments_items]
