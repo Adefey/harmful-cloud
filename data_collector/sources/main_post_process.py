cc8ba6baec820195c03cda3ca0eaae7ed3e744f4
Андрей Коленков <avoknelok@mail.ru>
2022-11-17 17:42:26 +0300

    СУБД работает

--- data_collector/sources/main_post_process.py
+++ data_collector/sources/main_post_process.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from sqlalchemy import create_engine
 
 from post_processor import json_to_dataframe
 
@@ -11,7 +12,19 @@ def main():
     with open(config["cache_filename"], "r", encoding="UTF-8") as file:
         raw_result_json = file.read()
     result = json_to_dataframe(raw_result_json)
-    result.to_csv(config["result_path"])
+    if config["save_to_disc"] == "yes":
+        result.to_csv(config["result_path"])
+    if config["save_to_mariadb"] == "yes":
+        user = config["MYSQL_USER"]
+        password = config["MYSQL_PASSWORD"]
+        host = "mariadb"
+        database = config["MYSQL_DATABASE"]
+        port = 3306
+        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4"
+        print(SQLALCHEMY_DATABASE_URI)
+        engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_recycle=1800)
+        result.to_sql('posts', con=engine)
+        engine.dispose()
 
 
 if __name__ == "__main__":
