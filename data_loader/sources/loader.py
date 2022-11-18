54b88a693fe43f7807796a3518bbfcf8a272835e
Андрей Коленков <avoknelok@mail.ru>
2022-11-19 00:40:39 +0300

    Обновил лоадер

--- data_loader/sources/loader.py
+++ data_loader/sources/loader.py
@@ -13,17 +13,19 @@ class Loader:
         self.database_uri = f"mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}?charset=utf8mb4"
         self.engine = create_engine(self.database_uri, pool_recycle=1800)
 
-    def load_batch(self, count):
-        df = pd.read_sql(f'select post_text, comment_text FROM posts limit {self.offset}, {count}', con = self.engine)
-        self.offset += count
-        return df
+    def load_batch(self, offset, count):
+        df = pd.read_sql(f'select * FROM posts limit {offset}, {count}', con = self.engine)
+        X = list(df["post_text"])
+        y = list(df["comment_text"])
+        return (X, y)
 
     def __iter__(self):
         self.offset = 0
         return self
 
     def __next__(self):
-        df = self.load_batch(self.batch_size)
-        if len(df) == 0:
+        res = self.load_batch(self.offset, self.batch_size)
+        self.offset += self.batch_size
+        if len(res) == 0:
             raise StopIteration
-        return df
+        return res
