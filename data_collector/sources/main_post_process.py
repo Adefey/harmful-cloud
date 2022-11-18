255e9f6e21f0c4bd3ecc5e0ea28dae18272d811b
Андрей Коленков <avoknelok@mail.ru>
2022-11-18 22:27:46 +0300

    Добавляю логи чтобы понять ошибку

--- data_collector/sources/main_post_process.py
+++ data_collector/sources/main_post_process.py
@@ -10,21 +10,26 @@ def main():
     with open(config_path, "r", encoding="UTF-8") as file:
         config = json.load(file)
     with open(config["cache_filename"], "r", encoding="UTF-8") as file:
+        logging.info(f"Start reading file")
         raw_result_json = file.read()
+        logging.info(f"File is read")
     result = json_to_dataframe(raw_result_json)
     if config["save_to_disc"] == "yes":
+        logging.info(f"Saving to CSV file")
         result.to_csv(config["result_path"])
+        logging.info(f"CSV file is saved")
     if config["save_to_mariadb"] == "yes":
         user = config["MYSQL_USER"]
         password = config["MYSQL_PASSWORD"]
         host = "mariadb"
         database = config["MYSQL_DATABASE"]
-        port = 3310
+        port = config["MYSQL_TCP_PORT"]
+        logging.info(f"Connecting to database")
         SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4"
-        print(SQLALCHEMY_DATABASE_URI)
         engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_recycle=1800)
         result.to_sql('posts', con=engine)
         engine.dispose()
+        logging.info(f"Data is saved to database")
     print("SUCCESS")
 
 
--- data_collector/sources/post_processor.py
+++ data_collector/sources/post_processor.py
@@ -2,7 +2,8 @@ import json
 import re
 from tqdm import tqdm
 import pandas as pd
-
+import logging
+logging.basicConfig(filename="logs.txt", filemode='a', format="%(asctime)s %(message)s", datefmt="%I:%M:%S %p", level=logging.INFO)
 
 def clean_string(string):
     permitted_chars = "^0-9A-Za-zА-Яа-яёЁ.!?/@#()*+-"
@@ -11,7 +12,9 @@ def clean_string(string):
 
 
 def json_to_dataframe(json_string):
+    logging.info(f"Start parsing JSON")
     data = json.loads(json_string)
+    logging.info(f"JSON is parsed")
     data_frame_list = []
     for post, comments in tqdm(data.items()):
         for comment in comments:
@@ -19,5 +22,7 @@ def json_to_dataframe(json_string):
                 if post != "":
                     data_frame_list.append({"post_text": clean_string(
                         post), "comment_text": clean_string(comment)})
+    logging.info(f"Start transforming to dataframe with {len(data_frame_list)} entries")
     data_frame = pd.DataFrame(data_frame_list)
+    logging.info(f"Dataframe is ready with {len(data_frame)} entries")
     return data_frame
